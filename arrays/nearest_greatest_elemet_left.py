7bef4ccf0e918fed43a34e55d5077fc196e6b454
Jagadeesh <47549598+jagadeeshgo@users.noreply.github.com>
2022-01-31 14:04:37 +0530

    Create nearest_greatest_elemet_left.py

---
+++ arrays/nearest_greatest_elemet_left.py
@@ -0,0 +1,18 @@
+arr=[1,0,3,2,4]
+stack=[]
+n=len(arr)
+res=[]
+for i in range(n):
+    if len(stack)==0:
+        res.append(-1)
+    elif len(stack)>0 and stack[-1]>arr[i]:
+        res.append(stack[-1])
+    elif len(stack)>0 and stack[-1]<=arr[i]:
+        while (len(stack)>0 and stack[-1]<=arr[i]):
+            stack.pop(-1)
+        if len(stack)==0:
+            res.append(-1)
+        else:
+            res.append(stack[-1])
+    stack.append(arr[i])
+print(res)
